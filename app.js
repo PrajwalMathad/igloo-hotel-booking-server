36a31aafe2bf986d469dae4dbf6fa66f91357783
PrajwalMathad <prajwalmathad966@gmail.com>
2023-12-07 23:12:52 -0500

    initial

---
+++ app.js
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import session from "express-session";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+const CONNECTION_STRING = 'mongodb+srv://prajwalmathad966:M0ng0Db%40966@cluster0.9xz8fmf.mongodb.net/kanbas?retryWrites=true&w=majority' || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
+app.use(express.json());
+// CourseRoutes(app);
+app.listen(process.env.PORT || 4000);
