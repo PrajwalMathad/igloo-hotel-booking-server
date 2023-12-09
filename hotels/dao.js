ac0d2edabd5fb2adfd4b49e2e753fea2222f7310
SatwikBhandiwad <satwik.bhandiwad@gmail.com>
2023-12-09 00:18:21 -0500

    create, update hotel apis

--- hotels/dao.js
+++ hotels/dao.js
@@ -3,3 +3,9 @@ export const createHotel = (hotel) => model.create(hotel);
 export const findAllHotels = () => model.find();
 export const findHotelsByCity = (city) =>
     model.find({ "location.city": city });
+
+export const findHotelById = (hotelId) => model.findById(hotelId);
+
+export const updateHotel = (hotelId, hotel) =>
+    model.updateOne({ hotel_id: hotelId }, { $set: hotel });
+
--- hotels/routes.js
+++ hotels/routes.js
@@ -18,8 +18,23 @@ function HotelRoutes(app) {
 
     };
 
+    const updateHotel = async (req, res) => {
+        const { hotelId } = req.params;
+        const status = await dao.updateHotel(hotelId, req.body);
+        currentHotel = await dao.findHotelById(hotelId);
+        res.json(status);
+    };
+
+    const createHotel = async (req, res) => {
+        const hotel = await dao.createHotel(req.body);
+        res.json(hotel);
+    };
+
+
+    app.post("/api/hotels", createHotel);
     app.get("/api/hotels/:city", findHotelsByCity);
     app.get("/api/hotels", findAllHotels);
+    app.put("/api/hotels/:hotelId", updateHotel);
 }
 
 export default HotelRoutes;
