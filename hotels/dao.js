bb847c2a9df76ca1a89da1a8a603260621e39c90
SatwikBhandiwad <satwik.bhandiwad@gmail.com>
2023-12-11 14:52:22 -0500

    Case insensitive search

--- hotels/dao.js
+++ hotels/dao.js
@@ -7,9 +7,11 @@ export const createHotel = (hotel) => {
 }
 export const findAllHotels = () => model.find();
 
-export const findHotelsByCity = (city) =>
-    model.find({ "location.city": city });
-
+export const findHotelsByCity = (city) =>{
+    const caseInsensitiveCity = new RegExp(`^${city}$`, 'i');
+    return model.find({ "location.city": caseInsensitiveCity });
+}
+    
 
 export const findHotelsByOwner = (owner) =>
     model.find({ "owner": owner });
